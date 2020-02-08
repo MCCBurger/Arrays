8daee282db500e2b8921c82d3f49725758946f09
Brian <bburger@students.mchenry.edu>
2020-02-08 13:37:54 -0600

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    //Declare array of supplies
+    var supplies = arrayOf("Regular Bun", "Poppy Seed Bun", "Hotdog", "Mustard", "Relish")
+    
+    //Print all supplies in list
+    for(x in supplies)
+        println(x)
+}
